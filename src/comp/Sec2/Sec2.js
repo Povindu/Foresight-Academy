b5cd4fae3de241c1eaf76f386ca05b9b5daf5c74
Povindu Samarasekara <povinduchanmith@gmail.com>
2023-08-07 21:43:24 +0530

    Changes

--- src/comp/Sec2/Sec2.js
+++ src/comp/Sec2/Sec2.js
@@ -6,7 +6,7 @@ const App = () => {
         <div className='sec2'>
             <div className="txt1">
                 <p className='txt11'>WELCOME TO</p>
-                <p className='txt12'>FORESIGHT ACADEMY</p>
+                <p className='txt12'>FORESIGHT<br/>ACADEMY</p>
                 <p className="txt13">At Foresight Academy, we believe in shaping the future through knowledge, collaboration, and innovation. Our mission is to foster an environment of continuous learning and foresight, empowering students to create a positive impact on society.</p>
             </div>
             <div className="txt2">
